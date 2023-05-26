a33fa33c990636ef2ae2fb23fdf11a34e3f15eb1
Ber5h <102763351+Ber5h@users.noreply.github.com>
2023-05-26 12:32:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -46,6 +46,8 @@ class Point:
         self.isHub = False
         self.isDeadEnd = False
         self.history = None
+    def output(self, string_temp):
+        print (self.x, self.y, string_temp)
 
 class LineString:
     def __init__(self, init_point, finish_point, history):
@@ -60,17 +62,14 @@ class LineString:
         self.isInVector = False
     def length(self):
         return distance(self.init_station, self.finish_station)
-    def output(self):
-        result = ''
-        for x in self.list_stations:
-            result+=x.name+' '
-        return result
+    def output(self, string_temp):
+        print(self.init_station.x, self.init_station.y, self.finish_station.x, self.finish_station.y, string_temp)
 
 class Vector:
     def __init__(self, list_points):
         self.list_points = list_points
-        self.init_station = None
-        self.finish_station = None
+        self.init_station = find_closest_station(list_points[0])
+        self.finish_station = find_closest_station(list_points[-1])
         self.list_result = self.history()
     def history(self):
         list_result = []
@@ -78,6 +77,8 @@ class Vector:
             if x.history!=None:
                 list_result.append(x.history)
         return list_result
+    def output(self):
+        print (self.init_station.name, '-', self.finish_station.name, 'VECTOR')
 
 def distance(point1, point2):
     return ((point1.x-point2.x)**2+(point1.y-point2.y)**2)**0.5
@@ -85,14 +86,17 @@ def distance(point1, point2):
 def count_for_point(list_temp, point_temp):
     result = 0
     for x in list_temp:
-        if x.x==point_temp.x and x.y == point_temp.y:
+        if distance(x, point_temp)<0.001:
             result +=1
     return result
 
 def create_list_vectors():
     vector_list = []
     for x in hub_list:
-        temp_direct = find_other_station_from_line(x, find_first_linestring(x))
+        x.output('hub')
+        temp_linestring = find_first_linestring(x)
+        temp_linestring.isInVector = True
+        temp_direct = find_other_station_from_line(x, temp_linestring)
         vector_list.append(create_vector(x, temp_direct))
     return vector_list
 
@@ -101,13 +105,20 @@ def create_vector(init_hub, direct_point):
     temp_points_list.append(init_hub)
     temp_points_list.append(direct_point)
     temp = direct_point
+    init_hub.output('init_hub')
+    direct_point.output('direct_point')
     while not temp.isHub and not temp.isDeadEnd:
-        print(temp.x, temp.y)
+        temp.output('temp')
         temp_linestring = find_first_linestring(temp)
         temp_linestring.isInVector = True
         temp = find_other_station_from_line(temp, temp_linestring)
         temp_points_list.append(temp)
-    return Vector(temp_points_list)
+    print('SUCCESS SUKA')
+    if temp.isDeadEnd:
+        print('Чурка')
+    vector_result = Vector(temp_points_list)
+    vector_result.output()
+    return vector_result
 
 def find_other_station_from_line(point1, line1):
     if is_point_in_line(point1, line1) == 1:
@@ -119,27 +130,33 @@ def find_first_linestring(point1):
     for x in main_linestring_list:
         if is_point_in_line(point1, x)>0 and not x.isInVector:
             return x
+        #if x.isInVector:
+        #    x.output('IsInVector')
     print(point1.x, point1.y, 'ERROR СУКА')
     if point1.isDeadEnd:
         print ('ГОВНО')
     return None
 
 def is_point_in_line(point1, line1):
-    if(point1.x == line1.init_station.x and point1.y == line1.init_station.y):
+    if(abs(point1.x-line1.init_station.x)<0.0001 and abs(point1.y - line1.init_station.y)<0.0001):
+        #line1.output('FUCK YOU')
         return 1
-    elif(point1.x == line1.finish_station.x and point1.y == line1.finish_station.y):
+    elif(abs(point1.x - line1.finish_station.x)<0.0001 and abs(point1.y - line1.finish_station.y)<0.0001):
+        #line1.output('FUCK YOU')
         return 2
     else:
         return 0
 
 def distance_mixed(point1, station1):
-    return distance(point1, Point(station1.longitude, station1.width, None))
+    return distance(point1, Point(station1.longitude, station1.width))
 
 def find_closest_station(point1):
     min_r = 5
     temp = None
     for x in main_list_station:
         temp_r = distance_mixed(point1, x)
+        if temp_r<=0:
+            print ('ПАРАША')
         if temp_r<min_r:
             min_r = temp_r
             temp = x
@@ -154,17 +171,19 @@ if __name__ == '__main__':
             main_list_station.append(
                 Station(row[0].value, row[1].value, row[2].value, row[3].value, row[4].value, row[5].value,
                         row[6].value, row[7].value))
-    linestring_file = openpyxl.load_workbook('linestring_data.xlsx')
+    linestring_file = openpyxl.load_workbook('linestring_Europe_data.xlsx')
     linestring_file = linestring_file['Sheet']
     main_linestring_list = []
     point_list = []
     index = 0
     for row in linestring_file:
         if row[0].value != 'x_init':
-            point_list.append(Point(row[0].value, row[1].value))
-            point_list.append(Point(row[2].value, row[3].value))
+            temp_point_first = Point(row[0].value, row[1].value)
+            point_list.append(temp_point_first)
+            temp_point_second = Point(row[2].value, row[3].value)
+            point_list.append(temp_point_second)
             main_linestring_list.append(
-                LineString(Point(row[0].value, row[1].value), Point(row[2].value, row[3].value), row[4].value))
+                LineString(temp_point_first, temp_point_second, row[4].value))
             print(index)
             index += 1
     hub_list = []
@@ -174,8 +193,10 @@ if __name__ == '__main__':
             hub_list.append(x)
             x.isHub = True
         if count_for_point(point_list, x) == 1:
+            x.output('DeadEnd')
             dead_end_list.append(x)
             x.isDeadEnd = True
+    print (len(dead_end_list), 'Dead_end_list')
     vector_list = create_list_vectors()
     for x in vector_list:
         x.init_station = find_closest_station(x.list_points[0])
